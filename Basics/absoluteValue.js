0ce480b8e1a7828e5ff470932895b6ceff2548fa
Darklord <dhirajkr1117@gmail.com>
2025-05-24 04:11:34 +0530

    basic codes competition questions

---
+++ Basics/absoluteValue.js
@@ -0,0 +1,6 @@
+function absoluteValue(num){
+    let ans =  num < 0 ? -num : num
+    return ans
+}
+let num= -8
+console.log(absoluteValue(num)) // 8
---
+++ Basics/celsiusToFarhenhiet.js
@@ -0,0 +1,6 @@
+function celsiusToFarhenhiet(celsius){
+    let farhenhiet =(celsius * 9/5)+32
+    return farhenhiet
+}
+let celsius= -40
+console.log(celsiusToFarhenhiet(celsius)) // 77
